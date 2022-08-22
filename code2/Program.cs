342981a2d2fb8507f5361d6e40872f9f39b0865d
Tatyana <Alehina8811@gmail.com>
2022-08-22 18:14:17 +0300

    задача2

--- code2/Program.cs
+++ code2/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberB * numberB;
+
+if (result == numberA)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
+
