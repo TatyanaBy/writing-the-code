7f6ade87460d5f90cb4d19f9ed994a6a0a087912
Tatyana <Alehina8811@gmail.com>
2022-08-22 18:36:40 +0300

    задача3

---
+++ code3/Program.cs
@@ -0,0 +1,29 @@
+﻿int week = Convert.ToInt32(Console.ReadLine());
+if(week == 1)
+{
+Console.WriteLine("Понедельник");
+}
+if(week == 2)
+{
+Console.WriteLine("Вторник");
+}
+if(week == 3)
+{
+Console.WriteLine("Среда");
+}
+if(week == 4)
+{
+Console.WriteLine("Четверг");
+}
+if(week == 5)
+{
+Console.WriteLine("Пятница");
+}
+if(week == 6)
+{
+Console.WriteLine("Суббота");
+}
+if(week == 7)
+{
+Console.WriteLine("Воскресенье");
+}
