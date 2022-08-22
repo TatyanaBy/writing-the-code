5a196034e5562ca6ffb668d04ee3f1b95dc34bf5
Tatyana <Alehina8811@gmail.com>
2022-08-22 17:10:44 +0300

    code1

---
+++ code1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number = Convert.ToInt32(Console.ReadLine()); // Конвертация в число.
+int result = number * number;
+Console.WriteLine(result);
