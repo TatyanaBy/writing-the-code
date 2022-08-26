5ac11f62cdcb42a89b6eaca768d4097d38708d70
Tatyana <Alehina8811@gmail.com>
2022-08-26 11:55:51 +0300

    Задача 12

---
+++ code8/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 12. Напишите программу, которая на вход будет принимать два числа и выводить. является ли второе число кратным первому. 
+//Если число 2 не кратно числу 1, то программы выводит остаток от деления
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a % b;
+if (result == 0)
+{
+Console.Write("Кратно");
+}
+else
+{
+int ostatok = a % b;
+Console.Write(ostatok);
+}
