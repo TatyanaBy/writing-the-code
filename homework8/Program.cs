cbcffbf0d2d8ae15045cd5706452780d983130eb
Tatyana <Alehina8811@gmail.com>
2022-08-31 18:22:27 +0300

    Задача 23

---
+++ homework8/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int N);
+
+
+if (isNumberN != true)
+{
+Console.WriteLine("Не число");
+return;
+}
+PrintArray (GetArray(N));
+int[] GetArray (int n)
+{
+    int i = 1;
+    int[] result = new int[n];
+    while(i <= n)
+    {
+        result[i-1] = (int) Math.Pow (i,3);
+        i++;
+    }
+    return result;
+}
+void PrintArray(int[] Array )
+{
+    int i = 0;
+    int len = Array.Length;
+     while(i < len)
+     {
+        Console.WriteLine(Array[i]);
+        i++;
+     }
+}
+
