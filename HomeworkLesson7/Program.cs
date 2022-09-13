d6dfb1f85eada7e1163e89d6c465c87cf2fdacc2
Tatyana <Alehina8811@gmail.com>
2022-09-13 11:16:13 +0300

    Задача 47

---
+++ HomeworkLesson7/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+CreateRandomArray(m, n);
+PrintTwoArray(array);
+
+void PrintTwoArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void CreateRandomArray(int m, int n)
+{
+    Random random = new Random();
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = Convert.ToDouble(random.Next(-100, 100)) / 10;
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает число и ищет в двумерном массиве, и возвращает индексы этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 7 -> 0 , 2
+// 5 -> 1 , 0
+// 18 -> нет такого элемента
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
