407d264be256a2343f0ebf5f554531d924fd1b0e
Tatyana <Alehina8811@gmail.com>
2022-09-14 18:28:59 +0300

    Задача 50

--- HomeworkLesson7/Program.cs
+++ HomeworkLesson7/Program.cs
@@ -3,38 +3,38 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-// int m = Convert.ToInt32(Console.ReadLine());
-// int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 
-// double[,] array = new double[m, n];
-// CreateRandomArray(m, n);
-// PrintTwoArray(array);
+double[,] array = new double[m, n];
+CreateRandomArray(m, n);
+PrintTwoArray(array);
 
-// void PrintTwoArray(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($" {array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// void CreateRandomArray(int m, int n)
-// {
-//     Random random = new Random();
-//     {
-//         for (int i = 0; i < array.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < array.GetLength(1); j++)
-//             {
-//                 array[i, j] = Convert.ToDouble(random.Next(-100, 100)) / 10;
-//             }
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void PrintTwoArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void CreateRandomArray(int m, int n)
+{
+    Random random = new Random();
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = Convert.ToDouble(random.Next(-100, 100)) / 10;
+            }
+        }
+        Console.WriteLine();
+    }
+}
 
 
 
@@ -46,55 +46,65 @@
 // 7 -> 0 , 2
 // 5 -> 1 , 0
 // 18 -> нет такого элемента
-// int m = Convert.ToInt32(Console.ReadLine());
-// int n = Convert.ToInt32(Console.ReadLine());
-// int[,] result = CreateRandomArray(m, n);
-// PrintTwoArray(result);
 
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+CreateRandomArray(numbers);
+PrintTwoArray(numbers);
 
-// // Console.Write("Введите число: ");
-// // int number = Convert.ToInt32(Console.ReadLine());
-// // string numberText = Convert.ToString(number);
-// // if (number == array || number == array)
-// // {
+void PrintTwoArray(int[,] array)  //печатаем массив
 
-// //   Console.WriteLine($"Индексы числа {number} в массиве:  ); + numberText[2] ");
-// // }
-// //  else 
-// // {
-// //   Console.WriteLine("Нет такого элемента");
-// // } 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
 
+void CreateRandomArray(int[,] array) //создаем массив
+{
+    // int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return;
+}
 
-// void PrintTwoArray(int[,] array)  //печатаем массив
+Console.Write("Введите число: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);  //принимаем число
+if (isNumber == false) //если ввели не число, а например какой-то символ..)
+{
+    Console.WriteLine("Это не число");
+    return;
+}
 
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($" {array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-
-// int[,] CreateRandomArray(int m, int n) //создаем рандомный массив, вводим заданные переменные m n
-// {
-//     int[,] array = new int[m, n]; //m столбец и n строка
-//     Random random = new Random();
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = random.Next(0, 10);
-//         }
-//     }
-//     return array;
-// }
-
-
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        if (number == numbers[i, j])
+        {
+            Console.WriteLine($"{number} -> {i}, {j}");
+        }
+    // else
+    //     {
+    //         Console.WriteLine("Элемента нет"); // так и не разобралась - когда нет элемента, 
+    //         //как сделать так, чтобы не выводилась информация по всем индексам, где реально нет элемента...Он по всем бегает и говорит, что здесь нет)
+    //     }
+    }
+}
 
 
 
@@ -155,7 +165,7 @@ for (int j = 0; j < number.GetLength(1); j++)
     avarage = avarage / m; //результат делим на количество элементов в столбце m
     Console.Write(avarage + " , ");
 }
-// Console.WriteLine();
+Console.WriteLine();
 
 
 
